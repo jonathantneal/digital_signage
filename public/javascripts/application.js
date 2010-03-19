9346cb6612dd444353e8e0fd57b6810e183e3311
Adam Crownoble <adam.crownoble@biola.edu>
2010-03-19 10:20:29 -0700

    Converting from Prototype to jQuery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,8 +3,5 @@ function add_fields(link, association, content) {
 
  var new_id = new Date().getTime();
  var regexp = new RegExp('new_' + association, 'g')
- $(association).insert({
-  bottom: content.replace(regexp, new_id)
- });
-
+ $('#'+association).append(content.replace(regexp, new_id));
 }
