93ab59559af91bb117002d50003bf81eaefd8fec
Adam Crownoble <adam.crownoble@biola.edu>
2011-06-09 15:23:36 -0700

    Removing rogue comma in admin_data initializer

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,4 +1,4 @@
 AdminData.config do |config|
-  config.is_allowed_to_view = lambda { |controller| controller.try(:current_user).try(:is_admin?) },
+  config.is_allowed_to_view = lambda { |controller| controller.try(:current_user).try(:is_admin?) }
   config.is_allowed_to_update = lambda { |controller| controller.try(:current_user).try(:is_admin?) }
 end
