0a8eeb7abe626437655bf32342a94c3c4b888c4e
Adam Crownoble <adam.crownoble@biola.edu>
2011-08-17 12:04:50 -0700

    Fixing javascripts paths using the ROOT_URL variables

--- public/javascripts/announcements.js
+++ public/javascripts/announcements.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
   var datepickers = $('dd.show_at input[type=text]');
   if(datepickers.length) { datepickers.datepicker({dateFormat: "MM d, yy '12:00 AM'"}); }
   var announcement = $('dd.announcement textarea');
-  if(announcement.length) { announcement.wysiwyg({css: ROOT_URL+'stylesheets/wysiwyg.css'}); }
+  if(announcement.length) { announcement.wysiwyg({css: ROOT_URL+'/stylesheets/wysiwyg.css'}); }
 });
