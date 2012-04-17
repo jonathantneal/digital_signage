93e65bd760ee0dabd65d28e6e31cf20294515ea9
Ryan Hall <ryan.e.hall@biola.edu>
2012-04-17 14:49:12 -0700

    Upgrade biola_web_services to latest version

--- config/initializers/biola_web_services.rb
+++ config/initializers/biola_web_services.rb
@@ -1,5 +1,8 @@
-BiolaWebServices.cert_path = AppConfig.security.cert_path
-BiolaWebServices.key_path = AppConfig.security.key_path
-BiolaWebServices.key_password = AppConfig.security.key_password
-BiolaWebServices.verify_ssl = AppConfig.security.verify_ssl
-BiolaWebServices.url = AppConfig.security.url
+BiolaWebServices.configure do |config|
+  config.url          = AppConfig.security.url
+  config.cert_path    = AppConfig.security.cert_path
+  config.key_path     = AppConfig.security.key_path
+  config.key_password = AppConfig.security.key_password
+  config.verify_ssl   = AppConfig.security.verify_ssl
+  config.ssl_version  = AppConfig.security.ssl_version
+end
