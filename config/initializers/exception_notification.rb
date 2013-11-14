2889ffb3b32df5cc8cfa9c6741d8e65ad66bf780
Ryan Hall <ryan.hall@biola.edu>
2013-11-14 12:44:12 -0800

    Fixed exception notification configuration.

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,8 @@
-if defined? ::ExceptionNotifier
-  SignManager::Application.config.middleware.use ::ExceptionNotifier,
-    :email_prefix => "[#{Settings.app.name}] ",
-    :sender_address => Settings.email.from,
-    :exception_recipients => [Settings.email.developer]
+if defined? ::ExceptionNotification
+  SignManager::Application.config.middleware.use ExceptionNotification::Rack,
+    email: {
+      email_prefix: "[#{Settings.app.name}] ",
+      sender_address: Settings.email.from,
+      exception_recipients: Settings.email.developer
+    }
 end
