6efae324a68c43b17ff9adbdc315d0f4bfb88d99
Michael Stephens <michael.a.stephens@biola.edu>
2014-04-15 12:18:45 -0700

    Added sidekiq check for pinglish

--- config/initializers/pinglish.rb
+++ config/initializers/pinglish.rb
@@ -3,6 +3,10 @@ SignManager::Application.config.middleware.use Pinglish do |ping|
     ActiveRecord::Base.connected?
   end
 
+  ping.check :redis do
+    Sidekiq.redis { |r| r.ping == 'PONG' }
+  end
+
   ping.check :smtp do
     smtp = Net::SMTP.new(ActionMailer::Base.smtp_settings[:address])
     smtp.start
